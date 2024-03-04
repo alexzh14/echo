0c7b58e23f2f31ffae166c8338b730ffb5022ccf
Alex Zh <alzh147@gmail.com>
2024-03-04 21:02:47 +0500

    Initial commit

---
+++ src/test/java/ru/netology/echo/TestEcho.java
@@ -0,0 +1,24 @@
+package ru.netology.echo;
+
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class TestEcho {
+
+    @Test
+    void shouldReturnSentData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Should go and find the answer by yourself")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("Should go and find the answer by yourself"));
+
+
+    }
+
+}
